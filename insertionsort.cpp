17301346531d383387a9a3c8d30dd2c48ae1c47c
Finem50 <Finem50@live.com>
2018-10-17 23:35:44 -0500

    Beginning project with insertion sort. Included minimal frame that needs to be filled out for basic functionality.

---
+++ insertionsort.cpp
@@ -0,0 +1,23 @@
+/** Requirements:
+-Linked list to be used for insertion sort
+-File in/out to be used for reading integers and building a linked list
+in the order that matches the file, and then sort that linked list.
+-All movement to be done by pointers
+-Data in nodes may not be changed after initially stored
+**/
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+
+using namespace std;
+
+
+int main(){
+
+
+
+
+  return 0;
+}
